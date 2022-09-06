b9e52b281eb5f1f5eaf3945719dbbab573e73999
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-09-06 14:47:30 +0500

    Initialize vuetify 3.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'vuetify/styles' // Global CSS has to be imported
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
-
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import './assets/main.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.use(vuetify)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,22 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <div>
+    <v-alert type="success">I'm a success alert.</v-alert>
+
+    <br>
+
+    <v-alert type="info">I'm an info alert.</v-alert>
+
+    <br>
+
+    <v-alert type="warning">I'm a warning alert.</v-alert>
+
+    <br>
+
+    <v-alert type="error">I'm an error alert.</v-alert>
+  </div>
+
   <main>
     <TheWelcome />
   </main>
